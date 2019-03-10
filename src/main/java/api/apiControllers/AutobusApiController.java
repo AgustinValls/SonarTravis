a3efafe6f0b5542e54b77e4ba83de323d43e5029
ATorrecilla <a.torrecilla@alumnos.upm.es>
2019-03-10 18:18:30 +0100

    modificacione apiController

--- src/main/java/api/apiControllers/AutobusApiController.java
+++ src/main/java/api/apiControllers/AutobusApiController.java
@@ -10,6 +10,7 @@ public class AutobusApiController {
     public static final String AUTOBUSES = "/autobuses";
 
     public static final String ID_ID = "/{id}";
+    public static final String MYID = "ID";
 
     public static final String LINEAREGULAR = "/lineaRegular";
 
